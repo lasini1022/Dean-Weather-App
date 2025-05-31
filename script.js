2724135ccbc11bcb80d35fa24daecc3810f4123d
lasini1022 <102490645+lasini1022@users.noreply.github.com>
2025-05-30 21:58:40 -0400

    added forecast display

--- script.js
+++ script.js
@@ -27,6 +27,16 @@ function changeCurrentCity(event) {
   axios.get(apiURL).then(changeInfo);
 }
 
+function defaultCity(city) {
+  let apiKey = "14b3e71od6df40977d863a00tdaeb6ef";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=" +
+    city +
+    "&key=" +
+    apiKey;
+  axios.get(apiURL).then(changeInfo);
+}
+
 function changeInfo(response) {
   console.log(response.data);
   let city = document.querySelector("#current-city");
@@ -45,9 +55,54 @@ function changeInfo(response) {
   let weather_text = document.querySelector("#weatherText");
   weather_text.innerHTML = response.data.condition.description;
 
-  // let icon_url = document.querySelector("#currentWeatherEmoji");
-  // icon_url.innerHTML = response.data.condition.icon_url;
+  let icon_url = document.querySelector("#currentWeatherEmoji");
+  icon_url.innerHTML =
+    "<img src=" +
+    response.data.condition.icon_url +
+    ' class="weather-app-icon" />';
+
+  getForecast(response.data.city);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "14b3e71od6df40977d863a00tdaeb6ef";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/forecast?query=" +
+    city +
+    "&key=" +
+    apiKey;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+        <div class="card day">
+          <div class="row days date">${formatDay(day.time)}</div>
+          <div class="row days"><img src=${day.condition.icon_url} /></div>
+          <div class="row">
+            <div class="col days"><strong>${Math.round(
+              day.temperature.maximum
+            )}°</strong> ${Math.round(day.temperature.minimum)}°</div>
+          </div>
+        </div>`;
+    }
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+defaultCity("Toronto");
 let searchButton = document.querySelector("#city-search-form");
 searchButton.addEventListener("submit", changeCurrentCity);
